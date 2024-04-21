b058505f353fbe5ea4e99397a5503536d8d05e41
oliverip <oliverivorp@gmail.com>
2024-04-21 12:40:17 -0700

    Cut down code, added functions, made it look nicer.

--- src/Blackjack.py
+++ src/Blackjack.py
@@ -1,17 +1,33 @@
-# Blackjack! Some of the code might be a bit bulky, I couldn't get some parts to work as condensed functions, but here it is.
+# Blackjack by Oliver Peterson 2024
 
 import random
-from deck import spades, diamonds, hearts, clubs, face_cards
-
-# Face cards
+from deck import spades, diamonds, hearts, clubs
 
 print("\nWelcome to Blackjack.\nYour goal is to have a higher hand than the dealer, without going over 21.")
 
 streak = 0
 
+suits = [spades, diamonds, hearts, clubs]
+
+money = 500
+
+def face_check(card):
+    '''Checks if a card is a face card, and adds the respective value'''
+    face_cards = ["Jack", "Queen", "King"]
+    if card in face_cards:
+        card = 10
+        return int(card)
+    elif card == "Ace":
+        if player_score < 11:
+            card = 11
+        else:
+            card = 1
+        return int(card)
+    else:
+        return int(card)
+
 playing = True
 while playing:
-    suits = [spades, diamonds, hearts, clubs]
 
     def draw_card():
         '''Randomly picks a suit, then picks a card from that suit.'''
@@ -24,22 +40,21 @@ while playing:
     dealer_2 = draw_card()
     dealer_hand_full = [dealer_1, dealer_2]
     dealer_hand = dealer_hand_full
-    # This was during the initial process, to make sure the dealer had the right amount of points for his cards.
-    # print(f"The Dealer has {dealer_1} and {dealer_2} in his hand.")
 
-    # This is to check if the dealer drew a face card. If he did, it will be converted into a numerical value.
+    # This is to check if the dealer has a face card. If they do, it will be converted into a numerical value.
 
     face_cards_dlr = {"Jack": 10, "Queen": 10, "King": 10, "Ace": 11}
     if dealer_1 in face_cards_dlr:
         dealer_1 = face_cards_dlr[dealer_1]
     if dealer_2 in face_cards_dlr:
         dealer_2 = face_cards_dlr[dealer_2]
-    print(dealer_2)
 
     dealer_total = int(dealer_1) + int(dealer_2)
 
     # Now, it's time for the player to draw cards!
-
+    print(f"Money: ${money}")
+    bet = int(input("How much would you like to bet? $"))
+    money = money - bet
     card_list = []
     player_score = 0
     first_card = draw_card()
@@ -47,34 +62,13 @@ while playing:
     second_card = draw_card()
     card_list.append(second_card)
 
+    print(f"Dealer's cards are {dealer_1} and ???")
     print(f"\nYour cards are {first_card} and {second_card}.")
 
     # Adding scores.
-    # Couldn't figure out how to work this into a function, I'll leave that for later.
-
-    if first_card in face_cards:
-        first_card = 10
-        player_score += 10
-    elif first_card == "Ace":
-        if player_score < 11:
-            first_card = 11
-        else:
-            first_card = 1
-        player_score += first_card
-    else:
-        player_score += first_card
-
-    if second_card in face_cards:
-        second_card = 10
-        player_score += 10
-    elif second_card == "Ace":
-        if player_score < 11:
-            second_card = 11
-        else:
-            second_card = 1
-        player_score += second_card
-    else:
-        player_score += second_card
+ 
+    player_score += face_check(first_card)
+    player_score += face_check(second_card)
 
     # At this point, the two cards have been drawn. Now, the player should draw cards until their total goes over 21.
 
@@ -84,32 +78,31 @@ while playing:
     while drawing:
         play = input(f"Would you like to draw another card? (Points: {player_score}) Y/N: ")
         if play.lower() == "n":
+
+            # Checking win conditions
+
             if player_score > dealer_total:
                 print(f"\nYour score:     {player_score}")
                 print(f"Dealer's score: {dealer_total}")
                 print("\nYou win!\n")
+                money = money + 2*bet
+                print(f"Money: ${money}")
                 streak += 1
                 drawing = False
             elif player_score < dealer_total:
                 print(f"\nYour score:     {player_score}")
                 print(f"Dealer's score: {dealer_total}")
                 print("\nYou lose.\n")
+                print(f"Money: ${money}")
                 streak = 0
+                if money == 0:
+                    print("You have run out of money. Thank you for playing!")
+                    playing = False
             drawing = False
         else:
             card = draw_card()
             print(f"You drew a {card}.")
-            if card in face_cards:
-                card = 10
-                player_score += 10
-            elif card == "Ace":
-                if player_score < 11:
-                    card = 11
-                else:
-                    card = 1
-                player_score += card
-            else:
-                player_score += card
+            player_score += face_check(card)
             print(f"\nYour point total is: {player_score}.")
             if player_score > 21:
                 print("Your score is over 21.\nYou lose.\n")
@@ -122,7 +115,8 @@ while playing:
     if cont.lower() == "y":
         playing = True
     else:
+        print(f"Final Money: ${money}")
         print("Thank you for playing!")
         playing = False
 
-# Reading the code, eh? Thanks for checking it out!
+# Reading the code? Thanks for checking it out!
--- src/deck.py
+++ src/deck.py
@@ -4,5 +4,3 @@ spades = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
 diamonds = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
 hearts = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
 clubs = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
-
-face_cards = ["Jack", "Queen", "King"]
